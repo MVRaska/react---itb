036782e2029a121e9597d46ca4eae2c012c90904
MVRaska <vujanacm@gmail.com>
2024-02-26 17:04:32 +0100

    cetvrti cas reducer

--- forms/src/cetvrti-cas/Reducer/BookContext.js
+++ forms/src/cetvrti-cas/Reducer/BookContext.js
@@ -6,7 +6,8 @@ export const BookContext = createContext();
 const defaultBooks = [
     {title: 'Ubiti pticu rugalicu', id: 1, author: 'Harper Li'},
     {title: 'Nepodnosljiva lakoca react-a', id: 2, author: 'Nikola Komazec'},
-    {title: 'Ko je maznuo moj sir', id: 3, author: 'Spenser Dzonson'}
+    {title: 'Ko je maznuo moj sir', id: 3, author: 'Spenser Dzonson'},
+    {title: 'Ispovest buntovnika', id: 4, author: 'Boris Njemcov'}
 ];
 
 const BookContextProvider = ({children}) => {
